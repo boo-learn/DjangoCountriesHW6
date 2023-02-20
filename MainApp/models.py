ffd67a57f4df1f38fc537d8727257a72639decab
Igor Kupriianov <igorkupriynov@yandex.ru>
2023-02-20 13:39:11 +0300

    Module-4 tasks complete.

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class Language(models.Model):
+   name = models.CharField(max_length=100)
+
+   def __repr__(self):
+      return f"language:{self.name}"
+
+
 class Country(models.Model):
     name = models.CharField(max_length=100)
     languages = models.TextField(max_length=1000)
+    language = models.ManyToManyField(to=Language)
+
+    def __repr__(self):
+        return f"country: {self.name} | {self.language}"
